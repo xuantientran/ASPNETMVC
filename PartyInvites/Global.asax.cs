7270b7e17807d54907fa58219c578a313637c468
TRAN Xuan Tien <txtien@gmail.com>
2019-04-29 07:04:41 +0200

    ASP MVC

--- PartyInvites/Global.asax.cs
+++ PartyInvites/Global.asax.cs
@@ -7,12 +7,12 @@ using System.Web.Routing;
 
 namespace PartyInvites
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
 }
